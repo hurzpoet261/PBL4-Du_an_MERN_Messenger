6de2373d64e5f4495e512d750274cf1b40ec2d4d
Nguyễn Hoàng Huy <huyk39a852@gmail.com>
2025-09-16 21:35:56 +0700

    Init project

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js"; 
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port :" + PORT);
+    connectDB();
+});
+
