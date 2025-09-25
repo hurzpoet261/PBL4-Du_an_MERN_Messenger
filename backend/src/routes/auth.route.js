6435cd2b648c6e0ad3599e40850a508b8ce600d6
Nguyễn Hoàng Huy <huyk39a852@gmail.com>
2025-09-25 10:38:37 +0700

    BE updateProfile

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { login, logout, signup } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { updateProfile } from '../controllers/auth.controller.js';
 
 const route = express.Router();
 
@@ -8,5 +10,7 @@ route.post('/signup', signup);
 route.post('/login',  login);
 
 route.post('/logout', logout);
+
+route.put('/update-profile', protectRoute, updateProfile);
 
 export default route;
