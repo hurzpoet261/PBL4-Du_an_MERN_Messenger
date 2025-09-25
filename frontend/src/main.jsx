bc2c99be9ea112b5efac5ef9bdbf8d5aa8d993e1
Tịnh Nguyễn Xuân <102230325@sv1.dut.udn.vn>
2025-09-25 10:38:48 +0700

    Tao cac page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as BrowseRoute } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowseRoute>
     <App />
+    </BrowseRoute>
   </StrictMode>,
 )
