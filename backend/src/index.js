b0963c95ad1847ca0eb55a1ec04f09055a40c178
Tịnh Nguyễn Xuân <102230325@sv1.dut.udn.vn>
2025-09-27 00:41:08 +0700

    add BE

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
