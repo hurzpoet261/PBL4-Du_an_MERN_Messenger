7c4d9c0f2266363712cc8d8ab92c58778a52ed5e
Nguyễn Hoàng Huy <huyk39a852@gmail.com>
2025-10-07 22:21:20 +0700

    fe fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,77 +1,52 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import Navbar from "./components/Navbar";
 
-// function App() {
-//   const [count, setCount] = useState(0)
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
 
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
+import { useEffect } from "react";
 
-// export default App
-import React from 'react'
-import {Routes, Route} from 'react-router-dom' 
-import { useAuthStore } from './store/useAuthStore'
-import { useEffect } from 'react'
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
-import Navbar from './components/Navbar'
-
-import HomePage from './pages/HomePage'
-import SignUpPage from './pages/SignUpPage'
-import LoginPage from './pages/LoginPage'
-import SettingsPage from './pages/SettingsPage'
-import ProfilePage from './pages/ProfilePage'
-
-import {Loader} from 'Lucide-react'
 const App = () => {
-  const {authUser, checkAuth, IsCheckingAuth} = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
+
+  console.log({ onlineUsers });
+
   useEffect(() => {
     checkAuth();
-  }, [authUser])
+  }, [checkAuth]);
 
-  console.log({authUser}); 
+  console.log({ authUser });
 
-  if(IsCheckingAuth && !authUser) return (
-    <div className='flex item-center justify-center h-screen'>
-      <Loader className= " size-10  animate-spin" />
-    </div>
-)
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
-    <div>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='signup' element={<SignUpPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/settings' element={<SettingsPage />} />
-        <Route path='/profile' element={<ProfilePage />} />
-      </Routes>
-    </div>
-  )
-}
+    <div data-theme={theme}>
+      <Navbar />
 
-export default App
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+};
+export default App;
